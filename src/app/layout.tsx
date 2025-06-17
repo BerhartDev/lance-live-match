2ed2c01a05f294c6d1570424348d3647c6734bbf
Bernardo Knoblauch <bekno.adm@gmail.com>
2025-06-16 22:33:59 -0300

    feat: vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next'
 import "./globals.css";
 import { ThemeProvider } from '@/theme/ThemeProvider'
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return <html lang="pt-BR">
     <body>
-    <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        {children}
+        <SpeedInsights />
+      </ThemeProvider>
     </body>
   </html>;
 }
