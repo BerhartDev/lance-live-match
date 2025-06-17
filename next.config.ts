a7e1f5253c07d4395a85e07ffbcf9722590dd48e
Bernardo Knoblauch <bekno.adm@gmail.com>
2025-06-17 00:08:21 -0300

    fix: imagedomains

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'temporeal.lance.com.br',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    unoptimized: true,
+    domains: ['temporeal.lance.com.br'],
   },
 };
 
